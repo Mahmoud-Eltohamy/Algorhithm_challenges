ac83f53aafa9395a1d9e29710ac3018f366df778
Mahmoud-Eltohamy <mahmoud.mohammed.elhady@gmail.com>
2018-10-13 21:09:39 +0200

    Update Program.cs

--- staircase/Program.cs
+++ staircase/Program.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,17 +7,38 @@ namespace staircase
 {
     class Program
     {
-        private static void staircase(int n)
+        //second method
+        private static void staircase1(int n)
         {
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine(new String(' ', n - i) + new String('#', i));
             }
         }
+        // first method
+        private static void staircase2(int n)
+        {
+            for (short i = 1; i <= n; ++i)
+            {
+                for (int j = 1; j <= n; ++j)
+                {
+                    if (j <= n - i)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            staircase(n);
+            staircase1(n);
+            staircase2(n);
             Console.ReadLine();
         }
     }
